11cef8d99cf09e56436cb4afdb7dce82e354fc33
tom andre-poyaud <tandrepoyaud@gmail.com>
2013-03-04 13:50:27 +0100

    WIP decoder

--- src/grtap/huffman/Decoder.java
+++ src/grtap/huffman/Decoder.java
@@ -4,51 +4,64 @@ import grtap.huffman.binarytree.Tree;
 import grtap.huffman.util.CharacterCode;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.TreeSet;
+import static grtap.huffman.Encoder.CHARSET;
 
-public abstract class Decoder {
+public class Decoder {
+	
+	private int treeEndPos;
+	private int separator;
 
-    public final static char    INCR_CODE_LENGTH = 0xB9;
-    public final static char    TREE_END         = 0xD7;	
-    public final static Charset CHARSET          = StandardCharsets.ISO_8859_1;
-	
-	
-	public static void decode(final Path from, final Path to, final boolean overwrite) throws IOException{
-        if (Files.notExists(from)) {
-            return; //TODO Error
-        } else if (Files.exists(to) && !overwrite) {
-            return; // TODO Error
-        }
+	public void decode(final Path from, final Path to,
+			final boolean overwrite) throws IOException {
+		if (Files.notExists(from)) {
+			return; // TODO Error
+		} else if (Files.exists(to) && !overwrite) {
+			return; // TODO Error
+		}
+
+		// First, we read the tree
+		try (final BufferedReader reader = Files.newBufferedReader(from, CHARSET)) {
+			treeEndPos = reader.read(); // first int in file is the position of first char after the tree
+			separator = reader.read(); // second int is separator
+
+			char[] treeString = new char[treeEndPos-2];
+
+			reader.read(treeString);
+
+			int[] array = new int[256]; // store chars and their code's length
+			int curLength = 1;
+
+			for (char c : treeString){
+				if (c == separator) {
+					curLength++;
+				} else {
+					array[c] = curLength;
+				}
+			}
+			
+		}
+
+		try(final FileChannel channel = new FileInputStream(from.toFile()).getChannel()){
+
+			MappedByteBuffer buffer = channel.map(FileChannel.MapMode.READ_ONLY, treeEndPos, channel.size());
+			
+		}
+
 		
-        //First, we read the tree
-        final BufferedReader reader = Files.newBufferedReader(from, CHARSET);
-        int[] array = new int[256]; //store chars and their code's length
-        Arrays.fill(array, 0); //a length of 0 => char not present
-        int curChar = reader.read();
-        int curLength = 1;
-        
-        while (curChar != TREE_END)
-        {
-        	while(curChar == INCR_CODE_LENGTH){
-        		curLength++;
-        		curChar = reader.read();
-        	}
-        	while(curChar != INCR_CODE_LENGTH && curChar != TREE_END){
-        		array[curChar] = curLength;
-        		curChar = reader.read();        		
-        	}
-        }
-        
-        //now we build the tree from the array
-        
-        //then we read the file and translate
+		
+		// now we build the tree from the array
+
+		// then we read the file and translate
 	}
 
-	
 }
