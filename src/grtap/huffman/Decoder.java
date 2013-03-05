45d98ca9b836f62b81ffabcf36d9bf8936c6bba9
tom andre-poyaud <tandrepoyaud@gmail.com>
2013-03-05 18:57:30 +0100

    Decoder lol

--- src/grtap/huffman/Decoder.java
+++ src/grtap/huffman/Decoder.java
@@ -17,99 +17,112 @@ import java.util.Map.Entry;
 
 public class Decoder {
 
+	private int treeEndPos;
+	private final Path from;
+	private final Path to;
+	private HashMap<BitArray, Character> codes;
 
-    private int                          treeEndPos;
-    private final Path                   from;
-    private final Path                   to;
-    private HashMap<BitArray, Character> codes;
+	public Decoder(final Path from, final Path to) {
+		this(from, to, false);
+	}
 
-    public Decoder(final Path from, final Path to) {
-        this(from, to, false);
-    }
+	public Decoder(final Path from, final Path to, final boolean overwrite) {
+		if (Files.notExists(from)) {
+			throw new IllegalArgumentException("Source file not found");
+		} else if (Files.exists(to) && !overwrite) {
+			throw new IllegalArgumentException(
+					"Destination file already exists");
+		}
 
-    public Decoder(final Path from, final Path to, final boolean overwrite) {
-        if (Files.notExists(from)) {
-            throw new IllegalArgumentException("Source file not found");
-        } else if (Files.exists(to) && !overwrite) {
-            throw new IllegalArgumentException("Destination file already exists");
-        }
+		this.from = from;
+		this.to = to;
+	}
 
-        this.from = from;
-        this.to = to;
-    }
-
-    public void decode() throws IOException {
+	public void decode() throws IOException {
 
         // first, we get the codes for each character
         codes = getCodes();
 
         // then we read the file and translate
 
-        try (final FileInputStream inputStream = new FileInputStream(from.toFile()); final FileChannel channel = inputStream.getChannel()) {
+        try (final FileInputStream reader = new FileInputStream(from.toFile()); 
+        		final BufferedWriter writer = Files.newBufferedWriter(to, CHARSET)) {
 
-            MappedByteBuffer buffer = channel.map(FileChannel.MapMode.READ_ONLY, treeEndPos, channel.size());
-            BufferedWriter writer = Files.newBufferedWriter(to, CHARSET);
-            char[] writeBuffer = new char[8192];
+            
+            char[] writeBuffer = new char[8192]; //TODO:
+            byte[] readBuffer = new byte[1024];
             int writeBufferPos = 0;
             Character curChar;
-            BitArray curCode = new BitArray(2);
-
-            for (int i = 0; i < buffer.capacity(); i++) { // read each byte in file
-                for (int j = 0; j < 7; j++) {
-                    curCode.add(0x80 & buffer.get(i) << j); // add each bit to current code
-                    if ((curChar = codes.get(curCode)) != null) { // if current code is a valid one
-                        writeBuffer[writeBufferPos++] = curChar; // add it to the writeBuffer
-                        if (writeBufferPos == 8192) {
-                            writer.write(writeBuffer); // write in destination file when writeBuffer is full
-                            writeBufferPos = 0; // reset
-                        }
-                    }
-                }
-            }
-            writer.write(writeBuffer, 0, writeBufferPos); // write remaining chars
+            BitArray curCode = new BitArray();
+            
+            int lengthByte;
+            
+            
+            do{
+            	lengthByte = reader.read(readBuffer);
+            	for(byte b : readBuffer){								// read each byte in file
+            		for(int i = 0; i < Byte.SIZE; i++){
+            			curCode.add((b&1<<i)==0?0:1);					// add each bit to current code
+            			if((curChar = codes.get(curCode)) != null){		// if current code is a valid one
+            				writeBuffer[writeBufferPos++] = curChar;	// add it to the writeBuffer
+            				if(writeBufferPos == 8192){
+            					writer.write(writeBuffer);				// write in destination file when writeBuffer is full
+            					writeBufferPos = 0;						// reset
+            				}
+            			}
+            		}
+            	}
+        		writer.write(writeBuffer,0,writeBufferPos);			// write remaining chars
+            }while(lengthByte == readBuffer.length);
         }
     }
 
-    private HashMap<BitArray, Character> getCodes() throws IOException {
-        HashMap<BitArray, Character> res = new HashMap<BitArray, Character>();
+	private HashMap<BitArray, Character> getCodes() throws IOException {
+		HashMap<BitArray, Character> res = new HashMap<BitArray, Character>();
 
-        // First, we read the tree
-        try (final BufferedReader reader = Files.newBufferedReader(from, CHARSET)) {
-            treeEndPos = reader.read(); // first int in file is the position of first char after the tree
+		// First, we read the tree
+		try (final BufferedReader reader = Files.newBufferedReader(from,
+				CHARSET)) {
+			treeEndPos = reader.read(); // first int in file is the position of
+										// first char after the tree
 
-            char[] treeString = new char[treeEndPos];
+			char[] treeString = new char[treeEndPos];
 
-            reader.read(treeString);
+			reader.read(treeString);
 
-            LinkedHashMap<Character, Integer> charCodesLength = new LinkedHashMap<Character, Integer>();// store chars and their code's length
+			LinkedHashMap<Character, Integer> charCodesLength = new LinkedHashMap<Character, Integer>();// store
+																										// chars
+																										// and
+																										// their
+																										// code's
+																										// length
 
-            for (int i = 0; i < treeString.length - 1; i += 2) {
-                charCodesLength.put(treeString[i], (int) treeString[i + 1]);
-            }
+			for (int i = 0; i < treeString.length - 1; i += 2) {
+				charCodesLength.put(treeString[i], (int) treeString[i + 1]);
+			}
 
-            BitArray curCode = new BitArray(2);
-            for (Entry<Character, Integer> e : charCodesLength.entrySet()) {
-                    for (int i = 0; i < e.getValue(); i++) {
-                        curCode.add(0);
-                        for(BitArray b : res.keySet()){
-                        	if(curCode.isPrefixedBy(b)){
-                                curCode.remove();
-                                curCode.add(1);
-                                break;
-                        	}
-                        }
-                    }
-                    for(BitArray b : res.keySet()){
-                    	while(b.isPrefixOf(curCode)){
-                    		curCode.increment();
-                    	}
-                    }
-                    res.put(curCode, e.getKey());
-                    curCode = new BitArray(2);
-                }
-        }
+			BitArray curCode = new BitArray(2);
+			boolean valid = false;
+			for (Entry<Character, Integer> e : charCodesLength.entrySet()) {
+				for (int i = 0; i < e.getValue(); i++) {
+					curCode.add(0);
+				}
+				do {
+					valid = true;
+					for (BitArray b : res.keySet()) {
+						if (b.isPrefixedBy(curCode) || curCode.isPrefixedBy(b)) {
+							curCode.increment();
+							valid = false;
+							break;
+						}
+					}
+				} while (!valid); // TODO : AU BUCHER
+				res.put(curCode, e.getKey());
+				curCode = new BitArray(2);
+			}
+		}
 
-        return res;
-    }
+		return res;
+	}
 
 }
