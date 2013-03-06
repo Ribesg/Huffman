9ca84473369e06c37b6310078e0cf13e996e031f
tom andre-poyaud <tandrepoyaud@gmail.com>
2013-03-06 13:35:55 +0100

    decoder should work

--- src/grtap/huffman/Decoder.java
+++ src/grtap/huffman/Decoder.java
@@ -1,7 +1,9 @@
 package grtap.huffman;
 
 import static grtap.huffman.Encoder.CHARSET;
+import grtap.huffman.binarytree.Tree;
 import grtap.huffman.util.BitArray;
+import grtap.huffman.util.CharacterCode;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -9,16 +11,16 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map.Entry;
+import java.util.TreeSet;
 
 public class Decoder {
 
-    private int                          treeEndPos;
+    private int                          treeLength;
     private final Path                   from;
     private final Path                   to;
-    private HashMap<BitArray, Character> codes;
+    private ArrayList<HashMap<BitArray, Character>> codes;
 
     public Decoder(final Path from, final Path to) {
         this(from, to, false);
@@ -38,7 +40,7 @@ public class Decoder {
     public void decode() throws IOException {
 
         // first, we get the codes for each character
-        codes = getCodes();
+        getCodes();
 
         // then we read the file and translate
 
@@ -48,72 +50,61 @@ public class Decoder {
             byte[] readBuffer = new byte[1024];
             int writeBufferPos = 0;
             Character curChar;
-            BitArray curCode = new BitArray();
-
-            reader.skip(treeEndPos);
-
-            int lengthByte;
-            lengthByte = reader.read(readBuffer);
-            while (lengthByte == readBuffer.length) {
-                for (byte b : readBuffer) { // read each byte in file
-                    for (int i = 0; i < Byte.SIZE; i++) {
-                        curCode.add((b & 1 << i) == 0 ? 0 : 1); // add each bit to current code
-                        if ((curChar = codes.get(curCode)) != null) { // if current code is a valid one
-                            writeBuffer[writeBufferPos++] = curChar; // add it to the writeBuffer
-                            if (writeBufferPos == 8192) {
-                                writer.write(writeBuffer); // write in destination file when writeBuffer is full
-                                writeBufferPos = 0; // reset
-                            }
-                        }
-                    }
-                }
-                lengthByte = reader.read(readBuffer);
+            BitArray curCode = new BitArray();            
+            
+            reader.skip(treeLength); 	//position the reader after the tree
+            
+            int lengthByte;            
+        	lengthByte = reader.read(readBuffer);            
+            while(lengthByte == readBuffer.length){
+        	for(byte b : readBuffer){															// read each byte in file
+            		for(int i = 0; i < Byte.SIZE; i++){
+            			curCode.add((b&1<<i)==0?0:1);											// add each bit to current code
+            			if((curChar = codes.get(curCode.length()).get(curCode)) != null){		// if current code is a valid one, i.e. present in our HashMap List
+        				writeBuffer[writeBufferPos++] = curChar;								// add it to the writeBuffer
+            				if(writeBufferPos == 8192){
+            					writer.write(writeBuffer);										// write in destination file when writeBuffer is full
+            					writeBufferPos = 0;												// reset
+            				}
+            			}
+            		}
+            	}
+            	lengthByte = reader.read(readBuffer);
             }
-            writer.write(writeBuffer, 0, writeBufferPos); // write remaining chars
+    		writer.write(writeBuffer,0,writeBufferPos);			// write remaining chars
         }
     }
 
-    private HashMap<BitArray, Character> getCodes() throws IOException {
-        HashMap<BitArray, Character> res = new HashMap<BitArray, Character>();
-
+    private void getCodes() throws IOException {
         // First, we read the tree
         try (final BufferedReader reader = Files.newBufferedReader(from, CHARSET)) {
-            // first int in file is the position of first char after the tree
-            treeEndPos = reader.read();
+            treeLength = reader.read(); // first int in file is the length of the tree, including this first int
 
-            char[] treeString = new char[treeEndPos];
+            char[] treeString = new char[treeLength-1];
 
             reader.read(treeString);
 
-            // store chars and their code's length
-            LinkedHashMap<Character, Integer> charCodesLength = new LinkedHashMap<Character, Integer>();
-
-            for (int i = 0; i < treeString.length - 1; i += 2) {
-                charCodesLength.put(treeString[i], (int) treeString[i + 1]);
+            TreeSet<CharacterCode> characterCodes = new Tree(treeString).getCharacterCodes();
+            int length = 0;
+            int l;
+            for(CharacterCode c : characterCodes){ //find maximum code length
+            	l=c.getCode().length();
+            	if(l > length){
+            		length = l;
+            	}
             }
-
-            BitArray curCode = new BitArray(2);
-            boolean valid = false;
-            for (Entry<Character, Integer> e : charCodesLength.entrySet()) {
-                for (int i = 0; i < e.getValue(); i++) {
-                    curCode.add(0);
-                }
-                do {
-                    valid = true;
-                    for (BitArray b : res.keySet()) {
-                        if (b.isPrefixedBy(curCode) || curCode.isPrefixedBy(b)) {
-                            curCode.increment();
-                            valid = false;
-                            break;
-                        }
-                    }
-                } while (!valid); // TODO : AU BUCHER
-                res.put(curCode, e.getKey());
-                curCode = new BitArray(2);
+            
+            codes = new ArrayList<HashMap<BitArray,Character>>(length); 
+            
+            for(CharacterCode c : characterCodes)
+            {
+            	l = c.getCode().length();
+            	if(codes.get(l) == null){
+            		codes.add(l, new HashMap<BitArray,Character>());
+            	}
+            	codes.get(l).put(c.getCode(), c.getChar());
             }
         }
-
-        return res;
     }
 
 }
